b24db857860249d3185ac1d1d08e4e3378c6b720
JohanOlson <62870685+JohanOlson@users.noreply.github.com>
2020-11-10 12:13:14 +0100

    Tisdag 10 Nov

--- Task 1.py
+++ Task 1.py
@@ -59,15 +59,15 @@ c2 = 0.25
  
 # Geometric inputs
 
-mI = 10 # number of mesh points X direction.
-mJ = 10 # number of mesh points Y direction.
+mI = 6 # number of mesh points X direction.
+mJ = 6 # number of mesh points Y direction.
 grid_type = 'equidistant' # this sets equidistant mesh sizing or non-equidistant
 xL = 1 # length of the domain in X direction
 yL = 1 # length of the domain in Y direction
 
 # Solver inputs
 
-nIterations  = 5 # maximum number of iterations
+nIterations  = 10 # maximum number of iterations
 resTolerance =  0.001 # convergence criteria for residuals each variable
 
 #====================== Code ======================
@@ -109,6 +109,7 @@ if grid_type == 'equidistant':
     # Control volume size
     dx = xL/(mI - 1)
     dy = yL/(mJ - 1)
+    
 
     # Fill the coordinates
     for i in range(mI):
@@ -126,14 +127,24 @@ if grid_type == 'equidistant':
                 yCoords_N[i,j] = 0.5*(yCoords_M[i,j] + yCoords_M[i,j-1]) # All y node coord.
             if j == (mJ-1) and i>0:
                 xCoords_N[i,j+1] = 0.5*(xCoords_M[i,j] + xCoords_M[i-1,j]) # last column of x, we miss without this one
+            if i == 0:
+                xCoords_N[i,j] = -dx/2
+            if j == 0:
+                yCoords_N[i,j] = -dy/2
+            if i == mI-1:
+                xCoords_N[nI-1,j] = xL+dx/2
+            if j == mJ-1:
+                yCoords_N[i,nJ-1] = yL+dy/2
+            
+            
 
             # Fill dx_CV and dy_CV
             if i>0:
                 dx_CV[i,j] = xCoords_M[i,j] - xCoords_M[i-1,j]
             if j>0:
                 dy_CV[i,j] = yCoords_M[i,j] - yCoords_M[i,j-1]
-                
-
+print(dx_CV)    
+print(dy_CV)            
 # =============================================================================
 # elif grid_type == 'non-equidistant':
 #     rx = 1.15
@@ -158,13 +169,14 @@ if grid_type == 'equidistant':
 
 # Initialize variable matrices and boundary conditions
                 
-    for i in range(1,nI-1):
-        T[i,0] = T1                                   #B1
-        T[i,nJ-1] = 5 + 3*(1+5*xCoords_N[i,nJ-1]/xL)  #B3
+for i in range(1,nI-1):
+     T[i,0] = T1                                   #B1
+     T[i,nJ-1] = 5 + 3*(1+5*xCoords_N[i,nJ-1]/xL)  #B3
 
-    for j in range(1,nJ-1):
-        T[nI-1,j] = T2                                #B2
-        # What about B4?!
+for j in range(1,nJ-1):
+     T[nI-1,j] = T2                                #B2
+     
+print(T)
 
 # Looping
         
@@ -178,70 +190,92 @@ for iter in range(nIterations):
     # Update conductivity coefficient matrix, k, according to your case
     for i in range(nI):
         for j in range(nJ):
-            k[i,j] = 2*(1+20*T[i,j]/T1)
+            k[i,j] = 2*(1+20*T[i,j]/T1)                         ############### FIX
         
             # Update source term matrix according to your case
-            S_U[i,j] = 15 * c1 * dx * dy    # In loop
-            S_P[i,j] = -15 * c2 * T[i,j] * dx *dy
+            #S_U[i,j] = 15 * (c1 -c2 * T[i,j]**2) * dx * dy    # In loop          ################# FIX
     
     # Compute coeffsT for all the nodes which are not boundary nodes
     ## Compute coefficients for nodes one step inside the domain
     
     ### First, north and south boundaries
     for i in range(2,nI-2):
-        # Why start at 2?! Should be 1?
-        coeffsT[i,1,2] = k_s * (xCoords_M[i,0] - xCoords_M[i-1,0])/(yCoords_N[i,1] - yCoords_N[i,0]) 
-        coeffsT[i,nJ-2,3] = k_n * (xCoords_M[i,-1] - xCoords_M[i-1,-1])/(yCoords_N[i,-1] - yCoords_N[i,nJ-2])
-
+        coeffsT[i,1,0] = k_w * dy_CV[i,1]/dx_CV[i,1]
+        coeffsT[i,1,1] = k_e * dy_CV[i,1]/dx_CV[i,1]
+        coeffsT[i,1,2] = 0 
+        S_U[i,1] += 2 * k_s * dx_CV[i,1] * T1 / dy_CV[i,1]                   # Work for non-equi?
+        coeffsT[i,1,3] = k_n * dx_CV[i,1]/dy_CV[i,1]
+        
+        coeffsT[i,nJ-2,0] = k_w * dy_CV[i,nJ-2]/dx_CV[i,nJ-2]
+        coeffsT[i,nJ-2,1] = k_e * dy_CV[i,nJ-2]/dx_CV[i,nJ-2]
+        coeffsT[i,nJ-2,2] = k_s * dx_CV[i,nJ-2]/dy_CV[i,nJ-2]
+        coeffsT[i,nJ-2,3] = 0
+        S_U[i,nJ-2] += 2 * k_n * dx_CV[i,nJ-2] * T[i,nJ-1] / dy_CV[i,nJ-2]    # Work for non-equi? T is T3
+        
     ### Second, east and west boundaries,  2, 4
     for j in range(2,nJ-2):
         coeffsT[1,j,0] = 0
-        coeffsT[-1,j,1] = k_e * (yCoords_M[-1,j+1] - yCoords_M[-1,j])/(xCoords_N[-1,j] - xCoords_N[nI-2,j])
+        S_U[1,j] = 0                     # Zero when neumann cond.
+        coeffsT[1,j,1] = k_e * dy_CV[1,j]/dx_CV[1,j]
+        coeffsT[1,j,2] = k_s * dx_CV[1,j]/dy_CV[1,j]
+        coeffsT[1,j,3] = k_n * dx_CV[1,j]/dy_CV[1,j]
+        
+        coeffsT[nI-2,j,0] = k_w * dy_CV[nI-2,j]/dx_CV[nI-2,j]
+        coeffsT[nI-2,j,1] = 0
+        S_U[nI-2,j] += 2 * k_e * dy_CV[nI-2,j] * T2 / dx_CV[nI-2,j]
+        coeffsT[nI-2,j,2] = k_s * dx_CV[nI-2,j]/dy_CV[nI-2,j]
+        coeffsT[nI-2,j,3] = k_n * dx_CV[nI-2,j]/dy_CV[nI-2,j]
         
     ## Compute coefficients for inner nodes
-    #TODO: FIX
     for i in range(2,nI-2):
-        for j in range(2,nJ-2): # This is not done for all!!!!!!, we also need the ones bordering the boundary
-            coeffsT[i,j,0] = k_w * (yCoords_M[i-1,j+1] - yCoords_M[i-1,j])/(xCoords_N[i,j] - xCoords_N[i-1,j])
-            coeffsT[i,j,1] = k_e * (yCoords_M[i+1,j+1] - yCoords_M[i+1,j])/(xCoords_N[i+1,j] - xCoords_N[i,j])
-            coeffsT[i,j,2] = k_s * (xCoords_M[i,j-1] - xCoords_M[i-1,j-1])/(yCoords_N[i,j] - yCoords_N[i,j-1])
-            coeffsT[i,j,3] = k_n * (xCoords_M[i,j+1] - xCoords_M[i-1,j+1])/(yCoords_N[i,j+1] - yCoords_N[i,j])
-              
-    
+        for j in range(2,nJ-2): # This is not done for all! # Index change? Unsure
+            coeffsT[i,j,0] = k_w * dy_CV[i,j]/dx_CV[i,j]
+            coeffsT[i,j,1] = k_e * dy_CV[i+1,j]/dx_CV[i+1,j]
+            coeffsT[i,j,2] = k_s * dx_CV[i,j]/dy_CV[i,j]
+            coeffsT[i,j,3] = k_n * dx_CV[i,j+1]/dy_CV[i,j+1]
+            
     ## Compute coefficients corner nodes (one step inside)
     # S-W corner
     coeffsT[1,1,0] = 0 
-    coeffsT[1,1,1] = k_e * (yCoords_M[2,2] - yCoords_M[2,1])/(xCoords_N[2,1] - xCoords_N[1,1])
-    coeffsT[1,1,2] = k_s * (xCoords_M[1,0] - xCoords_M[0,0])/(yCoords_N[1,1] - yCoords_N[1,0])
-    coeffsT[1,1,3] = k_n * (xCoords_M[1,2] - xCoords_M[0,2])/(yCoords_N[1,2] - yCoords_N[1,1])
+    S_U[1,1] += 0
+    coeffsT[1,1,1] = k_e * dy_CV[1,1]/dx_CV[1,1]
+    coeffsT[1,1,2] = 0
+    S_U[1,1] += 2 * k_s * dx_CV[1,1] * T1 / dy_CV[1,1]
+    coeffsT[1,1,3] = k_n * dx_CV[1,1]/dy_CV[1,1]
     
     # S-E corner
-    coeffsT[nI-2,1,0] = k_w * (yCoords_M[mI-3,2] - yCoords_M[mI-3,1])/(xCoords_N[nI-2,1] - xCoords_N[nI-3,1])
-    coeffsT[nI-2,1,1] = k_e * (yCoords_M[mI-1,2] - yCoords_M[mI-1,1])/(xCoords_N[nI-1,1] - xCoords_N[nI-2,1])   #####
-    coeffsT[nI-2,1,2] = k_s * (xCoords_M[mI-2,0] - xCoords_M[mI-3,0])/(yCoords_N[nI-2,1] - yCoords_N[nI-2,0])
-    coeffsT[nI-2,1,3] = k_n * (xCoords_M[mI-2,2] - xCoords_M[mI-3,2])/(yCoords_N[nI-2,2] - yCoords_N[nI-2,1])
+    coeffsT[nI-2,1,0] = k_w * dy_CV[nI-2,1]/dx_CV[nI-2,1]
+    coeffsT[nI-2,1,1] = 0
+    S_U[nI-2,1] += 2 * k_e * dy_CV[nI-2,1] * T2 / dx_CV[nI-2,1]    
+    coeffsT[nI-2,1,2] = 0
+    S_U[nI-2,1] += 2 * k_s * dx_CV[nI-2,1] * T1 / dy_CV[nI-2,1]  
+    coeffsT[nI-2,1,3] = k_n * dx_CV[nI-2,1]/dy_CV[nI-2,1]
     
     # N-W corner
-    coeffsT[1,nJ-2,0] = 0 
-    coeffsT[1,nJ-2,1] = k_e * (yCoords_M[2,mJ-1] - yCoords_M[2,mJ-2])/(xCoords_N[2,nJ-2] - xCoords_N[1,nJ-2])   #####
-    coeffsT[1,nJ-2,2] = k_s * (xCoords_M[1,mJ-3] - xCoords_M[0,mJ-3])/(yCoords_N[1,nJ-2] - yCoords_N[1,nJ-3])
-    coeffsT[1,nJ-2,3] = k_n * (xCoords_M[1,mJ-1] - xCoords_M[0,mJ-1])/(yCoords_N[1,nJ-1] - yCoords_N[1,nJ-2])   #####
+    coeffsT[1,nJ-2,0] = 0
+    S_U[1,nJ-2] += 0
+    coeffsT[1,nJ-2,1] = k_e * dy_CV[1,nJ-2]/dx_CV[1,nJ-2]
+    coeffsT[1,nJ-2,2] = k_s * dx_CV[1,nJ-2]/dy_CV[1,nJ-2]
+    coeffsT[1,nJ-2,3] = 0
+    S_U[1,nJ-2] += 2 * k_n * dx_CV[1,nJ-2] * T[1,nJ-2+1] / dy_CV[1,nJ-2] 
     
     # N-E corner
-    coeffsT[nI-2,nJ-2,0] = k_w * (yCoords_M[mI-3,mJ-1] - yCoords_M[mI-3,mJ-2])/(xCoords_N[nI-2,nJ-2] - xCoords_N[nI-3,nJ-2])
-    coeffsT[nI-2,nJ-2,1] = k_e * (yCoords_M[mI-1,mJ-1] - yCoords_M[mI-1,mJ-2])/(xCoords_N[nI-1,nJ-2] - xCoords_N[nI-2,nJ-2])
-    coeffsT[nI-2,nJ-2,2] = k_s * (xCoords_M[mI-2,mJ-3] - xCoords_M[mI-3,mJ-3])/(yCoords_N[nI-2,nJ-2] - yCoords_N[nI-2,nJ-3])
-    coeffsT[nI-2,nJ-2,3] = k_n * (xCoords_M[mI-2,mJ-1] - xCoords_M[mI-3,mJ-1])/(yCoords_N[nI-2,nJ-1] - yCoords_N[nI-2,nJ-2])
+    coeffsT[nI-2,nJ-2,0] = k_w * dy_CV[nI-2,nJ-2]/dx_CV[nI-2,nJ-2]
+    coeffsT[nI-2,nJ-2,1] = 0
+    S_U[nI-2,nJ-2] += 2 * k_e * dy_CV[nI-2,1] * T2 / dx_CV[nI-2,1]
+    coeffsT[nI-2,nJ-2,2] = k_s * dx_CV[nI-2,nJ-2]/dy_CV[nI-2,nJ-2]
+    coeffsT[nI-2,nJ-2,3] = 0
+    S_U[nI-2,nJ-2] += 2 * k_n * dx_CV[nI-2,nJ-2] * T[nI-2,nJ-2+1] / dy_CV[nI-2,nJ-2]
     
     # a_p
-    for i in range(1,nI-1):
-        for j in range(1,nJ-1):
+    for j in range(1,nJ-1):
+        for i in range(1,nI-1):
             coeffsT[i,j,4] = coeffsT[i,j,0] + coeffsT[i,j,1] + coeffsT[i,j,2] + coeffsT[i,j,3] - S_P[i,j]
-
+            
     # Solve for T using Gauss-Seidel
-    for i in range(1,nI-1):
-        for j in range(1,nJ-1):
-            T[i,j] = (S_U[i,j] + coeffsT[i,j,0]*T[i-1,j] + coeffsT[i,j,1]*T[i+1,j] + coeffsT[i,j,2]*T[i,j-1] + coeffsT[i,j,3]*T[i,j+1])/coeffsT[i,j,4]
+    for j in range(1,nJ-1):
+        for i in range(1,nI-1):
+            T[i,j] = (S_U[i,j] - coeffsT[i,j,0]*T[i-1,j] - coeffsT[i,j,1]*T[i+1,j] - coeffsT[i,j,2]*T[i,j-1] - coeffsT[i,j,3]*T[i,j+1])/(coeffsT[i,j,4])
     
     # Copy T to boundaries where homogeneous Neumann needs to be applied
     
@@ -256,10 +290,12 @@ for iter in range(nIterations):
     if resTolerance>residuals[-1]:
         break
 
-for j in range(nJ):
-    for i in range(nI):
-        print(T[i,j]) 
-    print("\n")
+# =============================================================================
+# for j in range(nJ):
+#     for i in range(nI):
+#         print(T[i,j]) 
+#     print("\n")
+# =============================================================================
 
 # Compute heat fluxes
 for i in range(1,nI-1):
@@ -268,7 +304,7 @@ for i in range(1,nI-1):
         q[i,j,1] = k[i,j] * (T[i,j+1] - T[i,j])/(yCoords_N[i,j+1] - yCoords_N[i,j])
     
 # Plotting section (these are some examples, more plots might be needed)
-
+print(T)
 # Plot results
 plt.figure()
 
